b63860e11f4889b96118585f381c1990f8337ad6
FLXNODE <119425934+fransiskuslasah01@users.noreply.github.com>
2024-02-03 19:27:23 +0800

    Create prover.sh

---
+++ prover.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+cd $HOME
+sudo apt-get update && sudo apt install git && sudo apt install screen
+
+echo -e "\e[1m\e[32m2. Open Port... \e[0m" && sleep 1
+ufw allow 22 && ufw allow 4133/tcp && ufw allow 3033/tcp && ufw enable
+
+echo -e "\e[1m\e[32m3. Install Rupstup... \e[0m" && sleep 1
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo -e "\e[1m\e[32m4. Clone Repository... \e[0m" && sleep 1
+cd $HOME
+git clone https://github.com/AleoHQ/snarkOS.git --depth 1
+
+echo -e "\e[1m\e[32m5. Build... \e[0m" && sleep 1
+cd snarkOS
+./build_ubuntu.sh
+
+echo -e "\e[1m\e[32m6. Cargo Path Install... \e[0m" && sleep 1
+apt install cargo
+curl https://sh.rustup.rs -sSf | sh
+source "$HOME/.cargo/env"
+cargo install cargo --force
+cargo install --path .
+
+echo -e "\e[1m\e[32m7. Create Account Aleo Prover... \e[0m" && sleep 1
+snarkos account new
+
+echo '=============== SETUP FINISHED BACKUP SEMUA DATA KALIAN, SIMPAN BAE BAE==================='
